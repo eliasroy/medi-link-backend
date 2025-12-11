33d21357a2e06d25ea66b095ada112d4ea12ec00
royelias <provinciaelias@gmail.com>
2025-12-11 16:49:28 -0500

    helth

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors()); //
 app.use(express.json());
-app.get("/", (_req, res) => {
+app.get("/health", (_req, res) => {
   res.status(200).json({ status: "ok" });
 });
 app.use("/api/auth", authRouter);
